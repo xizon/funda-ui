07753796ed16c215b996eb711f6b3e0a143a9708
UIUX Lab <uiuxlab@gmail.com>
2025-11-24 10:06:10 +0800

    update comment of useSSE()

--- packages/Utils/src/hooks/useSSE.tsx
+++ packages/Utils/src/hooks/useSSE.tsx
@@ -3,19 +3,44 @@
  * 
  * @usage:
  *
- * const App = () => {
- *     const { connected, messages, disconnect, reconnect } = useSSE('http://localhost:3000/sse');
- *
- *     return (
- *     <div>
- *         <p>Status: {connected ? '✅ Connected' : '❌ Disconnected'}</p>
- *         <button onClick={disconnect}>Disconnect</button>
- *         <button onClick={reconnect}>Reconnect</button>
- *         {messages.map((m, i) => <div key={i}>{m}</div>)}
- *     </div>
- *     );
- * };
+const App = () => {
+    const { connected, messages, disconnect, reconnect } = useSSE('http://localhost:3000/sse');
+
+    return (
+    <div>
+        <p>Status: {connected ? '✅ Connected' : '❌ Disconnected'}</p>
+        <button onClick={disconnect}>Disconnect</button>
+        <button onClick={reconnect}>Reconnect</button>
+        {messages.map((m, i) => <div key={i}>{m}</div>)}
+    </div>
+    );
+};
+
+ * It is recommended to use it in conjunction with usePageVisibility, because in HTTP mode, 
+ * browsers allow a maximum of 6 connections; otherwise, other normal interfaces will be suspended and inaccessible.
+
+ import usePageVisibility from 'funda-ui/Utils/usePageVisibility';
+
+const App = () => {
+    const { connected, messages, disconnect, reconnect } = useSSE('http://localhost:3000/sse');
+
+    // add new
+    usePageVisibility(
+        () => {
+            reconnect();
+        },
+        () => {
+            disconnect();
+        },
+        () => console.log("🎬 Page initialized while visible.")
+    );
+
+    return '';
+};
+
  */
+
+
 import { useEffect, useRef, useState, useCallback } from 'react';
 
 const useSSE = (url: string | null | undefined, retryDelay: number = 3000) => {
